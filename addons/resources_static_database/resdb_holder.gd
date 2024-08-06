5d9f36311037edf9d33878ab6596a6472a152be8
Orsopidou <61545749+Orso2p2n@users.noreply.github.com>
2024-08-06 17:59:42 +0200

    All holders in one file

--- addons/resources_static_database/resdb_holder.gd
+++ addons/resources_static_database/resdb_holder.gd
@@ -1,20 +1,21 @@
-extends Resource
+extends RefCounted
 
 class_name ResDbHolder
 
-var entries : Array
-var sub_holders : Array
+var entries : Array[ResDbEntry]
+var sub_holders : Array[ResDbHolder]
 
 var name : String
 var name_class : String
 
 var is_root = false
 
-func add_entry(entry_name : String, entry_extension : String, entry_type : String, entry_path : String):
+func add_entry(entry_name : String, entry_extension : String, entry_type : String, entry_path : String, uncertain_csharp_type : bool):
     var new_entry = ResDbEntry.new()
     new_entry.name = entry_name
     new_entry.extension = entry_extension
     new_entry.type = entry_type
+    new_entry.uncertain_csharp_type = uncertain_csharp_type
     new_entry.path = entry_path
 
     for entry in entries:
@@ -30,13 +31,32 @@ func add_sub_holder(sub_holder : ResDbHolder):
 
 func get_final_text() -> String:
     var text = ""
-    text += "extends Node\n\n"
-    text += "class_name %s\n\n" % [name_class]
+    var prefix = ""
+
+    if (is_root):
+        text += "class_name %s\n\n" % [name_class]
+    else:
+        text +="class %s:\n"  % [name_class]
+        prefix = "\t"
+
+    text += prefix + "extends Node\n\n"
 
     for holder in sub_holders:
-        text += "var %s : %s = %s.new()\n" % [holder.name, holder.name_class, holder.name_class]
+        text += prefix + "var %s : %s = %s.new()\n" % [holder.name, holder.name_class, holder.name_class]
 
     for entry in entries:
-        text += "var %s : %s = preload(\"%s\")\n" % [entry.name, entry.type, entry.path]
+        text += prefix + "var %s : %s = preload(\"%s\")\n" % [entry.name, entry.type, entry.path]
+
+    if (is_root):
+        text += add_sub_holders_text()
+
+    return text
+
+func add_sub_holders_text() -> String:
+    var text = "\n"
+
+    for sub_holder in sub_holders:
+        text += sub_holder.get_final_text()
+        text += sub_holder.add_sub_holders_text()
 
     return text
--- addons/resources_static_database/resources_database.gd
+++ addons/resources_static_database/resources_database.gd
@@ -12,22 +12,23 @@ var dock
 
 var old_db_name = ""
 
-var holders : Array
+var root_holder : ResDbHolder
 
 func _enter_tree():
+	# Set project settings for database
 	ProjectSettings.set_setting(ROOT_FOLDER_SETTING_PATH, "res://test/resources")
 	ProjectSettings.set_as_internal(ROOT_FOLDER_SETTING_PATH, true)
 
 	ProjectSettings.set_setting(DATABASE_NAME_SETTING_PATH, "Data")
 	ProjectSettings.set_as_internal(DATABASE_NAME_SETTING_PATH, true)
 
+	# Instantiate dock
 	dock = preload("res://addons/resources_static_database/scenes/resdb_dock.tscn").instantiate() as ResDbDock
 	dock.generate_button.pressed.connect(build_database)
 	add_control_to_dock(DOCK_SLOT_LEFT_BR, dock)
 
-	# ProjectSettings.settings_changed.connect(on_project_settings_changed)
-
 func _exit_tree():
+	# Remove everything when plugin is disabled
 	remove_old_autoload()
 	remove_control_from_docks(dock)
 	dock.free()
@@ -48,6 +49,7 @@ func build_database():
 	if (old_db_name != ""):
 		print_rich("Removing old Autoload [code]%s[/code]." % [old_db_name])
 
+	# Await a frame to remove the old autoload after the print
 	await get_tree().process_frame
 
 	remove_old_autoload()
@@ -55,32 +57,37 @@ func build_database():
 	print_rich("Clearing old generated database.")
 	clear_database()
 
+	# Force godot scan of files
 	force_scan()
 
 	if (db_name == ""):
 		printerr("Database Name is empty.")
 		return;
 
+	# Scan the root folder
 	if (!scan_dir(db_name, folder)):
 		return
 	
-	for holder in holders:
-		var file_name = holder.name_class.to_lower() + ".gd"
-		print_rich("[color=light_green]Creating file [code]%s[/code] of class [code]%s[/code]." % [file_name, holder.name_class])
-		var gen_file_path = GEN_FOLDER_PATH + "/" + file_name
-		var file = FileAccess.open(gen_file_path, FileAccess.WRITE)
-		var text = holder.get_final_text()
-		file.store_string(text)
+	# Write the main file
+	var file_name = root_holder.name_class.to_lower() + ".gd"
+	print_rich("[color=light_green]Creating file [code]%s[/code] of class [code]%s[/code]." % [file_name, root_holder.name_class])
+	var gen_file_path = GEN_FOLDER_PATH + "/" + file_name
+	var file = FileAccess.open(gen_file_path, FileAccess.WRITE)
+	var text = root_holder.get_final_text()
+	file.store_string(text)
 
+	# Force godot scan of files
 	force_scan()
 
-	var autoload_path = GEN_FOLDER_PATH + "/" + db_name.to_lower() + "tree.gd"
+	# Add autoload
+	var autoload_path = GEN_FOLDER_PATH + "/" + file_name
 	print_rich("Adding Autoload [code]%s[/code] with path [code]%s[/code]." % [db_name, autoload_path])
 
 	await get_tree().process_frame
 
 	add_autoload_singleton(db_name, autoload_path)
 
+	# Done!
 	print_rich("\n[color=light_green][b]Static Database generated![/b][/color]\n---")
 
 	old_db_name = db_name
@@ -92,7 +99,9 @@ func force_scan():
 		continue
 
 func clear_database():
-	holders = []
+	root_holder = null
+
+	# Delete old generated files
 	var dir = DirAccess.open(GEN_FOLDER_PATH)
 	for file in dir.get_files():
 		if file.ends_with(".gd"):
@@ -100,13 +109,21 @@ func clear_database():
 
 
 func scan_dir(dir_name : String, dir_path : String, parent_dir_name = "", depth = 0) -> bool:
+	# Prevent this from scanning stuff inside .godot
 	if (dir_path.contains(".godot")):
 		return false
 
+	# Prevent user from selecting a folder outside of res://
+	if (!dir_path.begins_with("res://")):
+		printerr("Root folder must be inside of res://")
+		return false
+	
+	# Stuff for prints
 	var indent_text = ""
 	for i in depth:
 		indent_text += "[indent]"
 
+	# Open folder
 	print_rich("%s[color=sky_blue]- Scanning folder [code]%s[/code]..." % [indent_text, dir_path])
 	var dir = DirAccess.open(dir_path)
 	var error = DirAccess.get_open_error()
@@ -114,16 +131,21 @@ func scan_dir(dir_name : String, dir_path : String, parent_dir_name = "", depth 
 		printerr("%s- Could not scan folder %s. Error: %s." % [indent_text, dir_path, error])
 		return false
 
+	# Create a holder for the current folder
 	create_holder(dir_name, parent_dir_name)
 
+	# Scan all the files in folder
 	dir.list_dir_begin()
 	var next = dir.get_next()
 	while next != "":
 		var next_path = dir_path + "/" + next
 
+		# If it's a directory, recursive call
 		if (dir.dir_exists(next)):
 			scan_dir(next, next_path, dir_name, depth + 1)
+		# Else, check if it's a resource
 		elif (ResourceLoader.exists(next_path)):
+			# Get all needed info
 			var res = ResourceLoader.load(next_path)
 			var split = next.split(".")
 			var name = split[0].to_snake_case()
@@ -131,24 +153,28 @@ func scan_dir(dir_name : String, dir_path : String, parent_dir_name = "", depth 
 
 			var type = res.get_class()
 			var script = res.get_script()
+			var uncertain_csharp_type = false
 			if (script != null):
 				var global_name = script.get_global_name()
 				if (global_name != ""):
 					type = global_name
 
-			if (!check_type(type)):
+			# Some types are illegal
+			if (!check_if_type_is_legal(type)):
 				next = dir.get_next()
 				continue
 
 			print_rich("[indent]%s[color=light_cyan]- Found resource [code]%s.%s[/code], of type [code]%s[/code], script %s." % [indent_text, name, extension, type, script])
-			create_entry(name, extension, type, next_path, dir_name)
+			
+			# Create entry for the resource
+			create_entry(name, extension, type, next_path, dir_name, uncertain_csharp_type)
 
 		next = dir.get_next()
 
 	return true
 
 
-func check_type(class_str : String) -> bool :
+func check_if_type_is_legal(class_str : String) -> bool :
 	match class_str:
 		"TextFile":
 			return false
@@ -173,23 +199,30 @@ func create_holder(name : String, in_holder = ""):
 
 	if (!is_root):
 		parent_holder.add_sub_holder(holder)
+	else:
+		root_holder = holder
 
-	holders.append(holder)
 
-
-func create_entry(entry_name : String, entry_extension : String, entry_type : String, entry_path : String, in_holder : String):
+func create_entry(entry_name : String, entry_extension : String, entry_type : String, entry_path : String, in_holder : String, uncertain_csharp_type : bool):
 	var holder = get_holder(in_holder)
 	if (holder == null):
 		return
 	
-	holder.add_entry(entry_name, entry_extension, entry_type, entry_path)
+	holder.add_entry(entry_name, entry_extension, entry_type, entry_path, uncertain_csharp_type)
 
 
 func get_holder(name : String) -> ResDbHolder:
 	if (name == ""):
-		return null;
+		return null
 
-	for holder in holders:
+	if (root_holder == null):
+		return null
+
+	if (root_holder.name == name):
+		return root_holder
+
+	for holder in root_holder.sub_holders:
 		if (holder.name == name):
 			return holder
+	
 	return null
